474cede957bb1901dec36cac256fa44c7d3a3745
Yogiraj Tambake <ytambake@gmail.com>
2024-12-17 19:04:18 +0530

    Added start.sh to start application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,57 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Scanner;
 
+@Slf4j
 public class Main {
+    private static String commandInfo = """
+            ## Commands
+            * `login [name]` - Logs in as this customer and creates the customer if not exist
+            * `deposit [amount]` - Deposits this amount to the logged in customer
+            * `withdraw [amount]` - Withdraws this amount from the logged in customer
+            * `transfer [target] [amount]` - Transfers this amount from the logged in customer to the target customer
+            * `logout` - Logs out of the current customer
+            * `exit` - Exits program
+            """;
+
     public static void main(String[] args) {
         ATM atm = new ATM();
         Display display = new Display(System.out);
         ATMController atmController = new ATMController(atm, display);
+        System.out.println(commandInfo);
 
         while (true) {
+            System.out.println("Waiting for your input\n");
             Scanner scanner = new Scanner(System.in);
             String command = scanner.next();
             switch (command) {
                 case "exit":
                     System.exit(0);
+                    break;
                 case "login":
                     String customerToLogin = scanner.next();
                     atmController.loginCustomer(customerToLogin);
+                    break;
                 case "logout":
                     String customerToLogout = scanner.next();
                     atmController.logoutCustomer(customerToLogout);
+                    System.out.println();
+                    break;
                 case "deposit":
                     int amountToDeposit = scanner.nextInt();
                     atmController.depositMoney(amountToDeposit);
+                    break;
                 case "withdraw":
                     int amountToWithdraw = scanner.nextInt();
                     atmController.withdrawMoney(amountToWithdraw);
+                    break;
                 case "transfer":
                     String transferTo = scanner.next();
                     int amountToTransfer = scanner.nextInt();
                     atmController.transferMoney(amountToTransfer, transferTo);
+                    break;
+                default:
+                    log.warn("Invalid command please input valid command");
             }
         }
     }
---
+++ start.sh
@@ -0,0 +1,2 @@
+./gradlew clean build jar
+java -jar ./build/libs/DKatalisProblem-1.0-SNAPSHOT.jar
